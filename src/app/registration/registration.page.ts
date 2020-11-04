b7dccf3efe7a985e7950e0864199510da0e7d77d
Peter Scanlon <pscanlon@tssg.org>
2020-11-04 11:36:25 +0000

    created registration screen

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationPage implements OnInit {
 
-  constructor() { }
+  constructor(private go: Router) {}
 
   ngOnInit() {
   }
 
+  register(){
+    this.go.navigate(['/login']);
+  }
 }
