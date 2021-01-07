d0e1039c538bca62303b64f4431fcac898c752f0
Peter Scanlon <pscanlon@tssg.org>
2021-01-07 12:16:48 +0000

    added configure params to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import Amplify, { Auth } from 'aws-amplify';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+Amplify.configure({
+  Auth:{
+    mandatorySignIn:true,
+    region: 'eu-west-1',
+    userPoolId: 'eu-west-1_dSJfazWPw',
+    userPoolWebClientId: '2hss4se424llruhcc82hg1rpd3',
+    authenticationFlowType:'USER_PASSWORD_AUTH'
+  }
+});
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
