34ad6ba7020c76a6032e6ff8afa7de30dfd281cc
Peter Scanlon <pscanlon@tssg.org>
2021-01-07 15:42:02 +0000

    basic login and registration implemented, requires more fine tuning

--- src/app/registration/registration.page.ts
+++ src/app/registration/registration.page.ts
@@ -1,19 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Auth } from 'aws-amplify';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
   styleUrls: ['./registration.page.scss'],
 })
 export class RegistrationPage implements OnInit {
-
+  firstName: string = '';
+  lastName: string = '';
+  college: string = '';
+  collegeYear: number = 1;
+  hospital: string = '';
+  email: string = '';
+  password: string = '';
+  passwordAgain: string = '';
   constructor(private go: Router) {}
 
   ngOnInit() {
   }
 
   register(){
+   try {
+     const user = Auth.signUp({
+      username: this.email,
+      password: this. password,
+      attributes: {
+        email: this.email,
+        given_name: this.firstName,
+        family_name: this.lastName,
+        'custom:hospital': this.hospital,
+        'custom:college': this.college,
+        'custom:collegeyear':this.collegeYear.toString()
+      }
+    });
+    console.log({ user });
+    alert('User signup complete, please check verify your email');
     this.go.navigate(['/login']);
+  } catch (error){
+    console.log('error signing up ', error);
   }
-}
+  }}
