ce08185143b7243ee14babf19effe49694825ec6
Peter Scanlon <pscanlon@tssg.org>
2021-01-08 11:20:22 +0000

    added service  for auth

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Auth } from 'aws-amplify';
+import { AuthenticationService } from '../services/authentication.service'
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
@@ -10,24 +9,12 @@ import { FormsModule } from '@angular/forms';
 export class LoginPage implements OnInit {
   email: string ='';
   password: string = '';
-  constructor(private go: Router) { }
+  constructor(private auth: AuthenticationService) { }
 
   ngOnInit() {
   }
 
   async login(){
-    try {
-      const user = await Auth.signIn(this.email.toString(), this.password.toString());
-      console.log('Authentication performed for user=' + this.email + 'password=' + this.password + ' login result==' + user);
-      const tokens = user.signInUserSession;
-      if (tokens != null){
-        console.log('User authenticated');
-        this.go.navigate(['home']);
-        alert('You are logged in successfully !');
-      }
-    } catch (error) {
-      console.log('Login Error ', error);
-      alert('User Authenication Failed');
-    }
+    this.auth.signIn(this.email, this.password);
   }
 }
---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,66 @@
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { Auth } from "aws-amplify";
+
+@Injectable({
+  providedIn: "root",
+})
+export class AuthenticationService {
+  constructor(private go: Router) {}
+
+  async signIn(username: string, password: string) {
+    try {
+      const user = await Auth.signIn(username.toString(), password.toString());
+      console.log('Authentication performed for user=' + username + 'password=' + password + ' login result==' + user);
+      const tokens = user.signInUserSession;
+      if (tokens != null){
+        console.log('User authenticated');
+        this.go.navigate(['home']);
+        alert('You are logged in successfully !');
+      }
+    } catch (error) {
+      console.log('Login Error ', error);
+      alert('User Authenication Failed');
+    }
+  }
+
+  register(email: string, password: string, firstName: string,
+    lastName: string, hospital?: string, college?: string, collegeYear?: number) {
+    try {
+      const user = Auth.signUp({
+        username: email,
+        password: password,
+        attributes: {
+          email: email,
+          given_name: firstName,
+          family_name: lastName,
+          "custom:hospital": hospital,
+          "custom:college": college,
+          "custom:collegeyear": collegeYear.toString(),
+        },
+      });
+      console.log({ user });
+      alert("User signup complete, please check verify your email");
+      this.go.navigate(["/login"]);
+    } catch (error) {
+      console.log("error signing up ", error);
+    }
+  }
+
+  async resendConfirmationCode(username: string) {
+    try {
+      await Auth.resendSignUp(username);
+      console.log("code resent successfully");
+    } catch (err) {
+      console.log("error resending code: ", err);
+    }
+  }
+
+  async signOut() {
+    try {
+      await Auth.signOut({ global: true });
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  }
+}
