0dd78c2feeb172c75aa4e900a9344ddf6d93a509
Peter Scanlon <pscanlon@tssg.org>
2021-02-22 12:39:35 +0000

    bug with goback

--- src/app/terms/terms.page.ts
+++ src/app/terms/terms.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Location} from '@angular/common'
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.page.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TermsPage implements OnInit {
 
-  constructor() { }
+  constructor(private loc: Location) { }
 
   ngOnInit() {
   }
 
+  goBack(){
+    this.loc.back();
+  }
 }
