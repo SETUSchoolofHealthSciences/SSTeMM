fa469ee1335ab10dbadc2d274b6502723f4cdc7f
Peter Scanlon <pscanlon@tssg.org>
2021-02-25 15:04:18 +0000

    styling around dropdown

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
+import { StorageService } from '../services/storage.service';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  selectedCountryCode = 'ie';
+  selectedCountryCode: string;
   countryCodes = ['ie', 'es', 'si'];
 
   formGroup: FormGroup;
   submitted = false;
-  constructor(public fb: FormBuilder, private auth: AuthenticationService) {
+  constructor(public fb: FormBuilder,
+              private auth: AuthenticationService,
+              private storage: StorageService,
+              private translate: TranslateService) {
     this.formGroup = fb.group({
       emailControl: [
         '',
@@ -30,9 +35,40 @@ export class LoginPage implements OnInit {
     });
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.storage.getLocalData('lang').then(data => {
+      switch (data) {
+        case 'en': {
+          this.selectedCountryCode = 'ie';
+          this.translate.use(data);
+          break;
+        }
+        case 'es': {
+          this.selectedCountryCode = data;
+          this.translate.use(data);
+          break;
+        }
+        case 'si': {
+          this.selectedCountryCode = data;
+          this.translate.use(data);
+          break;
+        }
+        default: {
+          this.selectedCountryCode = 'ie';
+          this.translate.use('en');
+        }
+      }
+    });
+  }
 
   changeSelectedCountryCode(value: string): void {
+    if (value === 'ie') {
+      this.storage.setLocalData('lang', 'en');
+      this.translate.use('en');
+    } else {
+      this.storage.setLocalData('lang', value);
+      this.translate.use(value);
+    }
     this.selectedCountryCode = value;
   }
 
