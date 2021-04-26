1be9d5dc8deda002fc041b00dc9fdd2ed2a6b589
Peter Scanlon <pscanlon@tssg.org>
2021-04-26 10:55:23 +0100

    added terms and conditions

--- src/app/terms/terms.page.ts
+++ src/app/terms/terms.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { StorageService } from '../services/storage.service';
 @Component({
   selector: 'app-terms',
   templateUrl: './terms.page.html',
@@ -7,12 +7,49 @@ import { Router } from '@angular/router';
 })
 export class TermsPage implements OnInit {
 
-  constructor(private go: Router) { }
-
+  constructor(private storage: StorageService) { }
+  de = false;
+  en = false;
+  es = false;
+  si = false;
   ngOnInit() {
-  }
-
-  goBack(){
-    this.go.navigate(['registration']);
+    this.storage.getLocalData('lang').then(data => {
+      switch (data) {
+        case 'en': {
+          this.en = true;
+          this.de = false;
+          this.es = false;
+          this.si = false;
+          break;
+        }
+        case 'es': {
+          this.en = false;
+          this.de = false;
+          this.es = true;
+          this.si = false;
+          break;
+        }
+        case 'si': {
+          this.en = false;
+          this.de = false;
+          this.es = false;
+          this.si = true;
+          break;
+        }
+        case 'de': {
+          this.en = false;
+          this.de = true;
+          this.es = false;
+          this.si = false;
+          break;
+        }
+        default: {
+          this.en = true;
+          this.de = false;
+          this.es = false;
+          this.si = false;
+        }
+      }
+    });
   }
 }
