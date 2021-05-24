10f88ccd04fd25fcbbf88e9b58bfdda4756d97c1
Peter Scanlon <pscanlon@tssg.org>
2021-05-24 08:54:28 +0100

    created profile page

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,19 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { ProfilePageRoutingModule } from './profile-routing.module';
-
 import { ProfilePage } from './profile.page';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { httpLoaderFactory } from '../app.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   declarations: [ProfilePage]
 })
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { CustomvalidationService } from '../services/customvalidation.service';
+import { TranslationService } from '../services/translation.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +13,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-
-  constructor() { }
+  formGroup: FormGroup;
+  submitted = false;
+  si = false;
+  constructor(public fb: FormBuilder,
+              private go: Router,
+              private auth: AuthenticationService,
+              private customValidator: CustomvalidationService,
+              private alertController: AlertController,
+              private translate: TranslationService,
+              private language: TranslateService) {
+    this.formGroup = fb.group(
+      {
+        firstNameControl: ['', Validators.compose([Validators.required])],
+        lastNameControl: ['', Validators.compose([Validators.required])],
+        collegeControl: [''],
+        collegeYearControl: [
+          '',
+          [Validators.maxLength(1), Validators.min(1), Validators.max(5)],
+        ],
+        collegeYearControlSi: [
+          '',
+          [Validators.maxLength(1), Validators.min(1), Validators.max(3)],
+        ],
+        hospitalControl: [''],
+      }
+    );
+   }
 
   ngOnInit() {
+    if (this.language.currentLang === 'si') {
+      this.si = true;
+    } else {
+      this.si = false;
+    }
+  }
+
+  get UpdateFormControl() {
+    return this.formGroup.controls;
   }
 
   ionViewWillEnter(){
