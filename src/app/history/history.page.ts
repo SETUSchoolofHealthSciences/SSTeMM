61b545d2770a22dcfe1989b6ff303b9fd988b268
Peter Scanlon <pscanlon@tssg.org>
2021-05-27 19:29:27 +0100

    filters added to application

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -8,10 +8,60 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['./history.page.scss'],
 })
 export class HistoryPage implements OnInit {
-
+  filter: string;
+  filters = [] as StressSignatue[];
   constructor(public api: ApiService) {
   }
 
   ngOnInit() {
+  }
+
+  ionViewDidEnter(){
+    this.filter = 'wk';
+  }
+
+  changeFilter() {
+    switch (this.filter) {
+      case 'wk': {
+        this.filters = [] as StressSignatue[];
+        const date = new Date();
+        date.setDate(date.getDate() - 7);
+        for (const item of this.api.allSignatures) {
+          if (item.timeStamp >= date.getTime()) {
+            this.filters.push(item);
+          }
+        }
+        break;
+      }
+      case 'mm': {
+        this.filters = [] as StressSignatue[];
+        const date = new Date();
+        date.setDate(date.getDate() - 28);
+        for (const item of this.api.allSignatures) {
+          if (item.timeStamp >= date.getTime()) {
+            this.filters.push(item);
+          }
+        }
+        break;
+      }
+      case 'year': {
+        this.filters = [] as StressSignatue[];
+        const date = new Date();
+        date.setDate(date.getDate() - 365);
+        for (const item of this.api.allSignatures) {
+          if (item.timeStamp >= date.getTime()) {
+            this.filters.push(item);
+          }
+        }
+        break;
+      }
+      case 'all': {
+        this.filters = this.api.allSignatures;
+        break;
+      }
+      default: {
+        this.filters = this.api.allSignatures;
+      }
+    }
   }
 }
